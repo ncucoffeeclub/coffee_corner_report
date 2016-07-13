55fe916c7003f3f4076660ee243554eaff7ec8c0
ncucoffeeclub <ncucoffeeclub@gmail.com>
2016-07-13 17:34:28 +0800

    test

--- js/custom.js
+++ js/custom.js
@@ -240,7 +240,7 @@ function submit(){
   })
 }
 
-var google_script_url = "https://script.google.com/macros/s/AKfycbzkiet3Bvvr_BC1V7kxFOowBejrDMreD9nI9FKY3mLwx_35Qe0l/exec"+"&callback=?";
+var google_script_url = "https://script.google.com/macros/s/AKfycbzkiet3Bvvr_BC1V7kxFOowBejrDMreD9nI9FKY3mLwx_35Qe0l/dev"+"&callback=?";
 
 function Sendresult(callback){
 
