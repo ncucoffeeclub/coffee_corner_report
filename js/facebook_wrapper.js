e2da84ee5af5eea5a651eec5e21ec91c6baf677e
ncucoffeeclub <ncucoffeeclub@gmail.com>
2016-07-13 11:14:28 +0800

    test

--- js/facebook_wrapper.js
+++ js/facebook_wrapper.js
@@ -75,6 +75,7 @@ function custom_login(callback){
       } else if (response.status === 'not_authorized') {
         callback(null,null);
       } else {
+        callback(null,null);
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
       }
